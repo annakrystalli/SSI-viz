b377d8be2d2522a934e94b81289f140782862774
annakrystalli <annakrystalli@googlemail.com>
2017-06-30 16:10:01 +0100

    commit render script + unpdate .gitignore

---
+++ render.R
@@ -0,0 +1,7 @@
+if (!require("pacman")) install.packages("pacman")
+pkgs <- c("rmarkdown", "knitr", "webshot")
+pacman::p_unload(pacman::p_loaded(), character.only = T)
+pacman::p_load(pkgs, character.only = T)
+#webshot::install_phantomjs()
+#knitr::spin("leaflet_map.R", format = c("Rmd"))
+rmarkdown::render("leaflet_map.R") 
